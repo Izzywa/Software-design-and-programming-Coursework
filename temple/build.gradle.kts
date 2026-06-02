1c84d986c4e05034dec9257a01c1b6093cc5c3c0
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-02 22:50:44 +0800

    build success with sdk21

--- temple/build.gradle.kts
+++ temple/build.gradle.kts
@@ -10,7 +10,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
-    id("io.freefair.lombok") version "8.0.1"
+    id("io.freefair.lombok") version "8.6"
     id("net.ltgt.errorprone") version "latest.release"
 }
 
@@ -29,7 +29,7 @@ dependencies {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(20))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
