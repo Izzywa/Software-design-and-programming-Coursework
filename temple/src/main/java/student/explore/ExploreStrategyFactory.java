370956957558ec4c9e35f9201bf06502be091fbc
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-27 14:31:48 +0800

    fix style and add getAllStrategies method for Strategy in ExploreStrategyFactory

--- temple/src/main/java/student/explore/ExploreStrategyFactory.java
+++ temple/src/main/java/student/explore/ExploreStrategyFactory.java
@@ -1,33 +1,55 @@
 package student.explore;
 
+import java.util.List;
+
+/**
+ * Factory class for creating instances of {@link ExploreStrategy} implementations.
+ */
 public class ExploreStrategyFactory {
-    public enum Strategy {
-        HeuristicDFS("HeuristicDFS"),
-        NaiveDFS("NaiveDFS"),
-        AStar("AStar");
-        
-        private final String name;
+  /**
+   * Returns an instance of the specified {@link ExploreStrategy} implementation.
+   *
+   * @param strategyName The name of the strategy to instantiate.
+   * @return An instance of the specified {@link ExploreStrategy}.
+   * @throws IllegalArgumentException If the strategy name is unknown.
+   */
+  public enum Strategy {
+    HeuristicDFS("HeuristicDFS"), NaiveDFS("NaiveDFS"), AStar("AStar");
 
-        Strategy(String name) {
-            this.name = name;
-        }
+    private final String name;
 
-        public String getName() {
-            return name;
-        }
+    Strategy(String name) {
+      this.name = name;
     }
-    public static ExploreStrategy getExploreStrategy(Strategy strategyName) {
-        switch (strategyName) {
-            case HeuristicDFS -> {
-                return new HeuristicDFSExploreStrategy();
-            }
-            case NaiveDFS -> {
-                return new NaiveDFSExploreStrategy();
-            }
-            case AStar -> {
-                return new AStarExploreStrategy();
-            }
-            default -> throw new IllegalArgumentException("Unknown strategy: " + strategyName);
-        }
+
+    public String getName() {
+      return name;
     }
+
+    public static List<Strategy> getAllStrategies() {
+      return List.of(HeuristicDFS, NaiveDFS, AStar);
+    }
+  }
+
+  /**
+   * Returns an instance of the specified {@link ExploreStrategy} implementation.
+   *
+   * @param strategyName The name of the strategy to instantiate.
+   * @return An instance of the specified {@link ExploreStrategy}.
+   * @throws IllegalArgumentException If the strategy name is unknown.
+   */
+  public static ExploreStrategy getExploreStrategy(Strategy strategyName) {
+    switch (strategyName) {
+      case HeuristicDFS -> {
+        return new HeuristicDFSExploreStrategy();
+      }
+      case NaiveDFS -> {
+        return new NaiveDFSExploreStrategy();
+      }
+      case AStar -> {
+        return new AStarExploreStrategy();
+      }
+      default -> throw new IllegalArgumentException("Unknown strategy: " + strategyName);
+    }
+  }
 }
