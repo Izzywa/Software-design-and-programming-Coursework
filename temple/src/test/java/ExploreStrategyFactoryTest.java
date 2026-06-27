85531ca62638da9a36acefa04217e2df4c26e42e
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-27 14:51:51 +0800

    add spaces for betterer readability

--- temple/src/test/java/ExploreStrategyFactoryTest.java
+++ temple/src/test/java/ExploreStrategyFactoryTest.java
@@ -5,19 +5,25 @@ import student.Explorer;
 import student.explore.ExploreStrategy;
 import student.explore.ExploreStrategyFactory;
 
+/** Tests for {@link ExploreStrategyFactory}. */
 public class ExploreStrategyFactoryTest {
   @Test
   public void testExploreStrategySetter() {
     Explorer explorer = new Explorer();
+
     ExploreStrategy newStrategy =
         ExploreStrategyFactory.getExploreStrategy(ExploreStrategyFactory.Strategy.NaiveDFS);
+
     explorer.setExploreStrategy(newStrategy);
+
     assertEquals(newStrategy, explorer.getExploreStrategy(),
         "The setter should update the exploration strategy.");
 
     ExploreStrategy anotherStrategy =
         ExploreStrategyFactory.getExploreStrategy(ExploreStrategyFactory.Strategy.AStar);
+
     explorer.setExploreStrategy(anotherStrategy);
+
     assertEquals(anotherStrategy, explorer.getExploreStrategy(),
         "The setter should update the exploration strategy to the new value.");
   }
@@ -25,8 +31,10 @@ public class ExploreStrategyFactoryTest {
   @Test
   public void testExploreStrategyDefault() {
     Explorer explorer = new Explorer();
+
     ExploreStrategy defaultStrategy =
         ExploreStrategyFactory.getExploreStrategy(ExploreStrategyFactory.Strategy.HeuristicDFS);
+        
     assertEquals(defaultStrategy.getClass(), explorer.getExploreStrategy().getClass(),
         "The default exploration strategy should be HeuristicDFS.");
   }
