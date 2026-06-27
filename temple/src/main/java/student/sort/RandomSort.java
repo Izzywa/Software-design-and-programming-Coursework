e4d51f739ab521e84eb958f5d5429bb7e42d52ef
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-27 21:23:03 +0800

    add random sort class

---
+++ temple/src/main/java/student/sort/RandomSort.java
@@ -0,0 +1,11 @@
+package student.sort;
+
+import java.util.List;
+
+public class RandomSort implements SortingStrategy {
+
+    @Override
+    public <T extends Comparable<T>> List<T> sort(List<T> list) {
+        throw new UnsupportedOperationException("RandomSort is not implemented yet.");
+    }
+}
