4436548530054f9daf416b259c372f3e70820643
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-27 14:09:32 +0800

    fix style for exploreStrategyFactoryTest

--- temple/src/test/java/ExploreStrategyFactoryTest.java
+++ temple/src/test/java/ExploreStrategyFactoryTest.java
@@ -6,22 +6,28 @@ import student.explore.ExploreStrategy;
 import student.explore.ExploreStrategyFactory;
 
 public class ExploreStrategyFactoryTest {
-    @Test
-    public void testExploreStrategySetter() {
-        Explorer explorer = new Explorer();
-        ExploreStrategy newStrategy = ExploreStrategyFactory.getExploreStrategy(ExploreStrategyFactory.Strategy.NaiveDFS);
-        explorer.setExploreStrategy(newStrategy);
-        assertEquals(newStrategy, explorer.getExploreStrategy(), "The setter should update the exploration strategy.");
+  @Test
+  public void testExploreStrategySetter() {
+    Explorer explorer = new Explorer();
+    ExploreStrategy newStrategy =
+        ExploreStrategyFactory.getExploreStrategy(ExploreStrategyFactory.Strategy.NaiveDFS);
+    explorer.setExploreStrategy(newStrategy);
+    assertEquals(newStrategy, explorer.getExploreStrategy(),
+        "The setter should update the exploration strategy.");
 
-        ExploreStrategy anotherStrategy = ExploreStrategyFactory.getExploreStrategy(ExploreStrategyFactory.Strategy.AStar);
-        explorer.setExploreStrategy(anotherStrategy);
-        assertEquals(anotherStrategy, explorer.getExploreStrategy(), "The setter should update the exploration strategy to the new value.");
-    }
+    ExploreStrategy anotherStrategy =
+        ExploreStrategyFactory.getExploreStrategy(ExploreStrategyFactory.Strategy.AStar);
+    explorer.setExploreStrategy(anotherStrategy);
+    assertEquals(anotherStrategy, explorer.getExploreStrategy(),
+        "The setter should update the exploration strategy to the new value.");
+  }
 
-    @Test
-    public void testExploreStrategyDefault() {
-        Explorer explorer = new Explorer();
-        ExploreStrategy defaultStrategy = ExploreStrategyFactory.getExploreStrategy(ExploreStrategyFactory.Strategy.HeuristicDFS);
-        assertEquals(defaultStrategy.getClass(), explorer.getExploreStrategy().getClass(), "The default exploration strategy should be HeuristicDFS.");
-    }
+  @Test
+  public void testExploreStrategyDefault() {
+    Explorer explorer = new Explorer();
+    ExploreStrategy defaultStrategy =
+        ExploreStrategyFactory.getExploreStrategy(ExploreStrategyFactory.Strategy.HeuristicDFS);
+    assertEquals(defaultStrategy.getClass(), explorer.getExploreStrategy().getClass(),
+        "The default exploration strategy should be HeuristicDFS.");
+  }
 }
