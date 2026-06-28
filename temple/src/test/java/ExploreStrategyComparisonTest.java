7d2ad0c65433631886ff09ba3acc4b38988da510
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-28 12:39:37 +0800

    use global var for num seed. increase number of seed and simplify seed iteration

--- temple/src/test/java/ExploreStrategyComparisonTest.java
+++ temple/src/test/java/ExploreStrategyComparisonTest.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,37 +10,33 @@ import student.explore.ExploreStrategyFactory;
 import student.explore.ExploreStrategyFactory.Strategy;
 
 /**
- * Compares explore strategies across a set of random seeds and saves the results to CSV.
+ * Compares explore strategies across a set of random seeds and saves the
+ * results to CSV.
  */
 public class ExploreStrategyComparisonTest {
+
+  private final int NUM_SEEDS = 500;
+
   @Test
   public void testExploreStrategyAndSaveMultiplier() {
-    List<Long> seeds = new ArrayList<>();
 
-    for (int i = 0; i < 50; i++) {
-      long seed = new Random().nextLong();
-      seeds.add(seed);
-    }
-
-    List<Strategy> strategies =
-        new ArrayList<>(Arrays.asList(ExploreStrategyFactory.Strategy.values()));
+    List<Strategy> strategies = new ArrayList<>(Arrays.asList(ExploreStrategyFactory.Strategy.values()));
 
     String filename = "explore_strategy_comparison.csv";
-    String[] headers = {"Strategy", "Seed", "Bonus Factor"};
+    String[] headers = { "Strategy", "Seed", "Bonus Factor" };
     List<String[]> results = new ArrayList<>();
 
     for (Strategy strategy : strategies) {
-      for (long seed : seeds) {
+      for (int seed = 0; seed < NUM_SEEDS; seed++) {
         MockGameState state = new MockGameState(seed, false);
         state.explorer.setExploreStrategy(ExploreStrategyFactory.getExploreStrategy(strategy));
 
         state.explore();
         double bonusFactor = state.computeBonusFactor();
         results.add(
-            new String[] {strategy.getName(), String.valueOf(seed), String.valueOf(bonusFactor)});
+            new String[] { strategy.getName(), String.valueOf(seed), String.valueOf(bonusFactor) });
       }
     }
-
     LogToCsv.saveToCsv(filename, headers, results);
   }
 }
