1cae05e5926dc919950012d40a527af8c4cc6189
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-29 13:35:08 +0800

    remove custom title from the plot

--- python/PlotExploreComparisonGraph.py
+++ python/PlotExploreComparisonGraph.py
@@ -14,7 +14,6 @@ if not images_dir.exists():
     os.makedirs(images_dir)
 
 df.boxplot(column="Bonus Factor", by="Strategy", grid=True, vert=False, figsize=(10, 5))
-plt.title("Explore Strategy Comparison")
 plt.xlabel("Bonus Factor")
 plt.ylabel("Strategy")
 plt.yticks(rotation=45)
