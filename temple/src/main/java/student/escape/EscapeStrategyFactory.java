73610af2dffcd01ce89ce7a92227cf7c723bf2ec
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-29 20:06:11 +0800

    add getEscapeStrategy method to the factory

--- temple/src/main/java/student/escape/EscapeStrategyFactory.java
+++ temple/src/main/java/student/escape/EscapeStrategyFactory.java
@@ -3,26 +3,57 @@ package student.escape;
 import lombok.Getter;
 
 /**
- * Factory class for creating instances of {@link EscapeStrategy} implementations.
+ * Factory class for creating instances of {@link EscapeStrategy}
+ * implementations.
  */
 public class EscapeStrategyFactory {
 
     /**
-     * 
+     * The enumeration of available exploration strategies.
      */
     @Getter
-  public enum Strategy {
-    BFS("BFS"),
-    DFSAllPaths("DFSAllPaths"),
-    DFSPruning("DFSPruning"),
-    Dijkstra("Dijkstra"),
-    KnapsackDFS("KnapsackDFS");
+    public enum Strategy {
+        BFS("BFS"),
+        DFSAllPaths("DFSAllPaths"),
+        DFSPruning("DFSPruning"),
+        Dijkstra("Dijkstra"),
+        KnapsackDFS("KnapsackDFS");
 
-    private final String name;
+        private final String name;
 
-    Strategy(String name) {
-      this.name = name;
+        Strategy(String name) {
+            this.name = name;
+        }
+
     }
 
-  }
+    /**
+     * Returns an instance of the specified {@link EscapeStrategy}
+     * implementation.
+     *
+     * @param strategyName The name of the strategy to instantiate.
+     * @return An instance of the specified {@link EscapeStrategy}.
+     * @throws IllegalArgumentException If the strategy name is unknown.
+     */
+    public static EscapeStrategy getEscapeStrategy(Strategy strategyName) {
+        return switch (strategyName) {
+            case BFS -> {
+                yield new EscapeBreadthFirstSearch();
+            }
+            case DFSAllPaths -> {
+                yield new EscapeDFSAllPaths();
+            }
+            case DFSPruning -> {
+                yield new EscapeDFSPruning();
+            }
+            case Dijkstra -> {
+                yield new EscapeDijkstra();
+            }
+            case KnapsackDFS -> {
+                yield new EscapeKnapsackDFSBnB();
+            }
+            default -> throw new IllegalArgumentException(
+                    "Unknown strategy: " + strategyName);
+        };
+    }
 }
