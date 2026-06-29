00e7ff2a431d6be8f64bb468da3de28efc7a24e3
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-29 19:56:13 +0800

    Add enum for the escape strategy factory

--- temple/src/main/java/student/escape/EscapeStrategyFactory.java
+++ temple/src/main/java/student/escape/EscapeStrategyFactory.java
@@ -1,5 +1,23 @@
 package student.escape;
 
+/**
+ * Factory class for creating instances of {@link EscapeStrategy} implementations.
+ */
 public class EscapeStrategyFactory {
 
+    @Getter
+  public enum Strategy {
+    BFS("BFS"),
+    DFSAllPaths("DFSAllPaths"),
+    DFSPruning("DFSPruning"),
+    Dijkstra("Dijkstra"),
+    KnapsackDFS("KnapsackDFS");
+
+    private final String name;
+
+    Strategy(String name) {
+      this.name = name;
+    }
+
+  }
 }
