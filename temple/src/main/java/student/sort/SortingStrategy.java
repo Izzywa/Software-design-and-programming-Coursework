eccf02efb9bdec135246cba1aaec69efd35283ec
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-30 08:49:59 +0800

    fix indetation in SortingStrategy

--- temple/src/main/java/student/sort/SortingStrategy.java
+++ temple/src/main/java/student/sort/SortingStrategy.java
@@ -6,5 +6,5 @@ import java.util.List;
  * A strategy for sorting a list of comparable elements.
  */
 public interface SortingStrategy {
-  public <T extends Comparable<T>> List<T> sort(List<T> list);
+    public <T extends Comparable<T>> List<T> sort(List<T> list);
 }
