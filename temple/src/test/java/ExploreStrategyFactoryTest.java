c84aa538d6881efecee1a959d672ca6823125fd9
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-30 08:47:07 +0800

    fix indentation to ExploreStrategyFactoryTest

--- temple/src/test/java/ExploreStrategyFactoryTest.java
+++ temple/src/test/java/ExploreStrategyFactoryTest.java
@@ -20,90 +20,88 @@ import student.sort.RandomSort;
 
 /** Tests for {@link ExploreStrategyFactory}. */
 public class ExploreStrategyFactoryTest {
-  @Test
-  public void testExploreStrategySetter() {
-    Explorer explorer = new Explorer();
+    @Test
+    public void testExploreStrategySetter() {
+        Explorer explorer = new Explorer();
 
-    ExploreStrategy newStrategy =
-        ExploreStrategyFactory.getExploreStrategy(ExploreStrategyFactory.Strategy.NaiveDFS);
+        ExploreStrategy newStrategy = ExploreStrategyFactory
+                .getExploreStrategy(ExploreStrategyFactory.Strategy.NaiveDFS);
 
-    explorer.setExploreStrategy(newStrategy);
+        explorer.setExploreStrategy(newStrategy);
 
-    assertEquals(newStrategy, explorer.getExploreStrategy(),
-        "The setter should update the exploration strategy.");
+        assertEquals(newStrategy, explorer.getExploreStrategy(),
+                "The setter should update the exploration strategy.");
 
-    ExploreStrategy anotherStrategy =
-        ExploreStrategyFactory.getExploreStrategy(ExploreStrategyFactory.Strategy.AStar);
+        ExploreStrategy anotherStrategy = ExploreStrategyFactory
+                .getExploreStrategy(ExploreStrategyFactory.Strategy.AStar);
 
-    explorer.setExploreStrategy(anotherStrategy);
+        explorer.setExploreStrategy(anotherStrategy);
 
-    assertEquals(anotherStrategy, explorer.getExploreStrategy(),
-        "The setter should update the exploration strategy to the new value.");
-  }
-
-  @Test
-  public void testExploreStrategyDefault() {
-    Explorer explorer = new Explorer();
-
-    assertTrue(explorer.getExploreStrategy() instanceof HeuristicDFSExploreStrategy,
-        "The default exploration strategy should be an instance of HeuristicDFSExploreStrategy.");
-  }
-
-  @Test
-  public void testHeuristicDFSExploreStrategyFactory() {
-    assertTrue(
-        ExploreStrategyFactory.getExploreStrategy(
-            ExploreStrategyFactory.Strategy.HeuristicDFS) instanceof HeuristicDFSExploreStrategy,
-        "The factory should return an instance of HeuristicDFSExploreStrategy for the HeuristicDFS strategy.");
-  }
-
-  @Test
-  public void testNaiveDFSExploreStrategyFactory() {
-    assertTrue(
-        ExploreStrategyFactory.getExploreStrategy(
-            ExploreStrategyFactory.Strategy.NaiveDFS) instanceof NaiveDFSExploreStrategy,
-        "The factory should return an instance of NaiveDFSExploreStrategy for the NaiveDFS strategy.");
-  }
-
-  @Test
-  public void testAStarExploreStrategyFactory() {
-    assertTrue(
-        ExploreStrategyFactory.getExploreStrategy(
-            ExploreStrategyFactory.Strategy.AStar) instanceof AStarExploreStrategy,
-        "The factory should return an instance of AStarExploreStrategy for the AStar strategy.");
-
-  }
-
-  @Test
-  public void testDfsRandomSortExploreStrategyFactory() {
-    ExploreStrategy strategy = ExploreStrategyFactory
-        .getExploreStrategy(ExploreStrategyFactory.Strategy.HeuristicDFSWithRandomSort);
-
-    assertTrue(
-        strategy instanceof HeuristicDFSExploreStrategy,
-        "The factory should return an instance of HeuristicDFSExploreStrategy for the HeuristicDFSWithRandomSort strategy.");
-
-    assertTrue(
-        ((HeuristicDFSExploreStrategy) strategy).getSortingStrategy() instanceof RandomSort,
-        "The factory should return an instance of HeuristicDFSWithRandomSort with a RandomSort sorting strategy."
-    );
-  }
-
-  @RepeatedTest(50)
-  public void testAllStrategiesSucceedInReachingOrb() {
-    long seed = new Random().nextLong();
-
-    List<Strategy> strategies =
-        new ArrayList<>(Arrays.asList(ExploreStrategyFactory.Strategy.values()));
-    
-    for (Strategy strategy : strategies) {
-      MockGameState mockState = new MockGameState(seed, false);
-
-      mockState.explorer.setExploreStrategy(ExploreStrategyFactory.getExploreStrategy(strategy));
-      mockState.explore();
-
-      assertTrue(mockState.getExploreSucceeded(),
-          "The " + strategy.getName() + " strategy should reach the orb.");
+        assertEquals(anotherStrategy, explorer.getExploreStrategy(),
+                "The setter should update the exploration strategy to the new value.");
     }
-  }
+
+    @Test
+    public void testExploreStrategyDefault() {
+        Explorer explorer = new Explorer();
+
+        assertTrue(explorer.getExploreStrategy() instanceof HeuristicDFSExploreStrategy,
+                "The default exploration strategy should be an instance of HeuristicDFSExploreStrategy.");
+    }
+
+    @Test
+    public void testHeuristicDFSExploreStrategyFactory() {
+        assertTrue(
+                ExploreStrategyFactory.getExploreStrategy(
+                        ExploreStrategyFactory.Strategy.HeuristicDFS) instanceof HeuristicDFSExploreStrategy,
+                "The factory should return an instance of HeuristicDFSExploreStrategy for the HeuristicDFS strategy.");
+    }
+
+    @Test
+    public void testNaiveDFSExploreStrategyFactory() {
+        assertTrue(
+                ExploreStrategyFactory.getExploreStrategy(
+                        ExploreStrategyFactory.Strategy.NaiveDFS) instanceof NaiveDFSExploreStrategy,
+                "The factory should return an instance of NaiveDFSExploreStrategy for the NaiveDFS strategy.");
+    }
+
+    @Test
+    public void testAStarExploreStrategyFactory() {
+        assertTrue(
+                ExploreStrategyFactory.getExploreStrategy(
+                        ExploreStrategyFactory.Strategy.AStar) instanceof AStarExploreStrategy,
+                "The factory should return an instance of AStarExploreStrategy for the AStar strategy.");
+
+    }
+
+    @Test
+    public void testDfsRandomSortExploreStrategyFactory() {
+        ExploreStrategy strategy = ExploreStrategyFactory
+                .getExploreStrategy(ExploreStrategyFactory.Strategy.HeuristicDFSWithRandomSort);
+
+        assertTrue(
+                strategy instanceof HeuristicDFSExploreStrategy,
+                "The factory should return an instance of HeuristicDFSExploreStrategy for the HeuristicDFSWithRandomSort strategy.");
+
+        assertTrue(
+                ((HeuristicDFSExploreStrategy) strategy).getSortingStrategy() instanceof RandomSort,
+                "The factory should return an instance of HeuristicDFSWithRandomSort with a RandomSort sorting strategy.");
+    }
+
+    @RepeatedTest(50)
+    public void testAllStrategiesSucceedInReachingOrb() {
+        long seed = new Random().nextLong();
+
+        List<Strategy> strategies = new ArrayList<>(Arrays.asList(ExploreStrategyFactory.Strategy.values()));
+
+        for (Strategy strategy : strategies) {
+            MockGameState mockState = new MockGameState(seed, false);
+
+            mockState.explorer.setExploreStrategy(ExploreStrategyFactory.getExploreStrategy(strategy));
+            mockState.explore();
+
+            assertTrue(mockState.getExploreSucceeded(),
+                    "The " + strategy.getName() + " strategy should reach the orb.");
+        }
+    }
 }
