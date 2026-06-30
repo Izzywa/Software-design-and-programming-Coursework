81709a567fbd972c77c5e0cff78eef5b7b81eb48
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-30 20:09:53 +0800

    add repeated test to test all escape strategies

--- temple/src/test/java/EscapeStrategyFactoryTest.java
+++ temple/src/test/java/EscapeStrategyFactoryTest.java
@@ -1,28 +1,39 @@
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 import org.junit.jupiter.api.Test;
 
+import game.MockGameState;
 import student.Explorer;
 import student.escape.EscapeBreadthFirstSearch;
 import student.escape.EscapeDFSAllPaths;
 import student.escape.EscapeDFSPruning;
 import student.escape.EscapeDijkstra;
 import student.escape.EscapeKnapsackDFSBnB;
+import student.escape.EscapeKnapsackDFSDetour;
 import student.escape.EscapeStrategy;
 import student.escape.EscapeStrategyFactory;
+import student.escape.EscapeStrategyFactory.Strategy;
 
 public class EscapeStrategyFactoryTest {
 
     @Test
     public void testEscapeStrategyDefault() {
         Explorer explorer = new Explorer();
-        
+        assertTrue(
+                explorer.getEscapeStrategy() instanceof EscapeKnapsackDFSBnB);
     }
 
     @Test
     public void testGetBFSStrategy() {
         EscapeStrategy bfsStrategy = EscapeStrategyFactory.getEscapeStrategy(
-            EscapeStrategyFactory.Strategy.BFS
-        );
+                EscapeStrategyFactory.Strategy.BFS);
         assertTrue(bfsStrategy instanceof EscapeBreadthFirstSearch);
     }
 
@@ -52,5 +63,37 @@ public class EscapeStrategyFactoryTest {
         EscapeStrategy knapsackDFSStrategy = EscapeStrategyFactory
                 .getEscapeStrategy(EscapeStrategyFactory.Strategy.KnapsackDFS);
         assertTrue(knapsackDFSStrategy instanceof EscapeKnapsackDFSBnB);
+    }
+
+    @Test
+    public void testGetKnapsackDetourStrategy() {
+        EscapeStrategy knapsackDetourStrategy = EscapeStrategyFactory
+                .getEscapeStrategy(
+                    EscapeStrategyFactory.Strategy.KnapsackDetour
+                );
+        assertTrue(knapsackDetourStrategy instanceof EscapeKnapsackDFSDetour);
+    }
+
+    @RepeatedTest(10)
+    public void testAllStrategiesSucceedInEscaping() {
+        long seed = new Random().nextLong();
+
+        List<Strategy> strategies = new ArrayList<>(
+            Arrays.asList(EscapeStrategyFactory.Strategy.values())
+        );
+
+        for (Strategy strategy : strategies) {
+            MockGameState mockState = new MockGameState(seed, false);
+
+            mockState.explorer.setEscapeStrategy(
+                EscapeStrategyFactory.getEscapeStrategy(strategy)
+            );
+            mockState.explore();
+            mockState.escape();
+
+            assertTrue(mockState.getEscapeSucceeded(),
+                    "The " + strategy.getName()
+                    + " strategy should reach the orb.");
+        }
     }
 }
