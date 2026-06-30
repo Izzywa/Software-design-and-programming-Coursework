8beeec1f250dd541fd6f7853b21c0d208a39bbe6
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-30 08:49:23 +0800

    fix indentation in ExploreStrategyFactory

--- temple/src/main/java/student/explore/ExploreStrategyFactory.java
+++ temple/src/main/java/student/explore/ExploreStrategyFactory.java
@@ -4,47 +4,48 @@ import lombok.Getter;
 import student.sort.RandomSort;
 
 /**
- * Factory class for creating instances of {@link ExploreStrategy} implementations.
+ * Factory class for creating instances of {@link ExploreStrategy}
+ * implementations.
  */
 public class ExploreStrategyFactory {
-  /**
-   * Returns an instance of the specified {@link ExploreStrategy} implementation.
-   */
-  @Getter
-  public enum Strategy {
-    HeuristicDFS("HeuristicDFS"), NaiveDFS("NaiveDFS"), AStar("AStar"),
-    HeuristicDFSWithRandomSort("HeuristicDFSWithRandomSort");
+    /**
+     * Returns an instance of the specified {@link ExploreStrategy} implementation.
+     */
+    @Getter
+    public enum Strategy {
+        HeuristicDFS("HeuristicDFS"), NaiveDFS("NaiveDFS"), AStar("AStar"),
+        HeuristicDFSWithRandomSort("HeuristicDFSWithRandomSort");
 
-    private final String name;
+        private final String name;
 
-    Strategy(String name) {
-      this.name = name;
+        Strategy(String name) {
+            this.name = name;
+        }
+
     }
 
-  }
-
-  /**
-   * Returns an instance of the specified {@link ExploreStrategy} implementation.
-   *
-   * @param strategyName The name of the strategy to instantiate.
-   * @return An instance of the specified {@link ExploreStrategy}.
-   * @throws IllegalArgumentException If the strategy name is unknown.
-   */
-  public static ExploreStrategy getExploreStrategy(Strategy strategyName) {
-    return switch (strategyName) {
-      case HeuristicDFS -> {
-        yield new HeuristicDFSExploreStrategy();
-      }
-      case NaiveDFS -> {
-        yield new NaiveDFSExploreStrategy();
-      }
-      case AStar -> {
-        yield new AStarExploreStrategy();
-      }
-      case HeuristicDFSWithRandomSort -> {
-        yield new HeuristicDFSExploreStrategy(new RandomSort());
-      }
-      default -> throw new IllegalArgumentException("Unknown strategy: " + strategyName);
-    };
-  }
+    /**
+     * Returns an instance of the specified {@link ExploreStrategy} implementation.
+     *
+     * @param strategyName The name of the strategy to instantiate.
+     * @return An instance of the specified {@link ExploreStrategy}.
+     * @throws IllegalArgumentException If the strategy name is unknown.
+     */
+    public static ExploreStrategy getExploreStrategy(Strategy strategyName) {
+        return switch (strategyName) {
+            case HeuristicDFS -> {
+                yield new HeuristicDFSExploreStrategy();
+            }
+            case NaiveDFS -> {
+                yield new NaiveDFSExploreStrategy();
+            }
+            case AStar -> {
+                yield new AStarExploreStrategy();
+            }
+            case HeuristicDFSWithRandomSort -> {
+                yield new HeuristicDFSExploreStrategy(new RandomSort());
+            }
+            default -> throw new IllegalArgumentException("Unknown strategy: " + strategyName);
+        };
+    }
 }
