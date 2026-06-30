e58ecd7dcb031b16ef3d7388c1e0711296225ad8
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-06-30 12:25:46 +0800

    add javadoc for SortingStrategy sort

--- temple/src/main/java/student/sort/SortingStrategy.java
+++ temple/src/main/java/student/sort/SortingStrategy.java
@@ -6,5 +6,13 @@ import java.util.List;
  * A strategy for sorting a list of comparable elements.
  */
 public interface SortingStrategy {
-    public <T extends Comparable<T>> List<T> sort(List<T> list);
+  /**
+   * Sorts a list of comparable elements according
+   * to the strategy defined by the implementing class.
+   * @param <T> the type of elements in the list,
+   * which must implement Comparable
+   * @param list the list to sort
+   * @return a new list containing the sorted elements
+   */
+    <T extends Comparable<T>> List<T> sort(List<T> list);
 }
