c335ca3a5108d2bc075bd2ef6e8cced314157b9d
Izzatul Najwa binti Ismail <inbi1@student.london.ac.uk>
2026-07-01 13:25:05 +0800

    reinsert spacing from merge conflict

--- temple/src/main/java/student/explore/ExploreStrategyFactory.java
+++ temple/src/main/java/student/explore/ExploreStrategyFactory.java
@@ -4,69 +4,70 @@ import lombok.Getter;
 import student.sort.RandomSort;
 
 /**
- * Factory class for creating instances of {@link ExploreStrategy} implementations.
+ * Factory class for creating instances of {@link ExploreStrategy}
+ * implementations.
  */
 public class ExploreStrategyFactory {
-  /**
-   * The enumeration of available exploration strategies.
-   */
-  @Getter
-  public enum Strategy {
-    HeuristicDFS("HeuristicDFS"),
-    NaiveDFS("NaiveDFS"),
-    AStar("AStar"),
-    HeuristicDFSWithRandomSort("HeuristicDFSWithRandomSort"),
-    BeamSearch("BeamSearch"),
-    BFS("BFS"),
-    DynamicAStar("DynamicAStar"),
-    HillClimbing("HillClimbing"),
-    RandomWalk("RandomWalk");
+    /**
+     * The enumeration of available exploration strategies.
+     */
+    @Getter
+    public enum Strategy {
+        HeuristicDFS("HeuristicDFS"),
+        NaiveDFS("NaiveDFS"),
+        AStar("AStar"),
+        HeuristicDFSWithRandomSort("HeuristicDFSWithRandomSort"),
+        BeamSearch("BeamSearch"),
+        BFS("BFS"),
+        DynamicAStar("DynamicAStar"),
+        HillClimbing("HillClimbing"),
+        RandomWalk("RandomWalk");
 
-    private final String name;
+        private final String name;
 
-    Strategy(String name) {
-      this.name = name;
+        Strategy(String name) {
+            this.name = name;
+        }
+
     }
 
-  }
-
-  /**
-   * Returns an instance of the specified {@link ExploreStrategy} implementation.
-   *
-   * @param strategyName The name of the strategy to instantiate.
-   * @return An instance of the specified {@link ExploreStrategy}.
-   * @throws IllegalArgumentException If the strategy name is unknown.
-   */
-  public static ExploreStrategy getExploreStrategy(Strategy strategyName) {
-    return switch (strategyName) {
-      case HeuristicDFS -> {
-        yield new HeuristicDFSExploreStrategy();
-      }
-      case NaiveDFS -> {
-        yield new NaiveDFSExploreStrategy();
-      }
-      case AStar -> {
-        yield new AStarExploreStrategy();
-      }
-      case HeuristicDFSWithRandomSort -> {
-        yield new HeuristicDFSExploreStrategy(new RandomSort());
-      }
-      case BeamSearch -> {
-        yield new BeamSearchExploreStrategy();
-      }
-      case BFS -> {
-        yield new BreadthFirstExploreStrategy();
-      }
-      case DynamicAStar -> {
-        yield new DynamicAStarExploreStrategy();
-      }
-      case HillClimbing -> {
-        yield new HillClimbingExploreStrategy();
-      }
-      case RandomWalk -> {
-        yield new RandomWalkExploreStrategy();
-      }
-      default -> throw new IllegalArgumentException("Unknown strategy: " + strategyName);
-    };
-  }
+    /**
+     * Returns an instance of the specified {@link ExploreStrategy} implementation.
+     *
+     * @param strategyName The name of the strategy to instantiate.
+     * @return An instance of the specified {@link ExploreStrategy}.
+     * @throws IllegalArgumentException If the strategy name is unknown.
+     */
+    public static ExploreStrategy getExploreStrategy(Strategy strategyName) {
+        return switch (strategyName) {
+            case HeuristicDFS -> {
+                yield new HeuristicDFSExploreStrategy();
+            }
+            case NaiveDFS -> {
+                yield new NaiveDFSExploreStrategy();
+            }
+            case AStar -> {
+                yield new AStarExploreStrategy();
+            }
+            case HeuristicDFSWithRandomSort -> {
+                yield new HeuristicDFSExploreStrategy(new RandomSort());
+            }
+            case BeamSearch -> {
+                yield new BeamSearchExploreStrategy();
+            }
+            case BFS -> {
+                yield new BreadthFirstExploreStrategy();
+            }
+            case DynamicAStar -> {
+                yield new DynamicAStarExploreStrategy();
+            }
+            case HillClimbing -> {
+                yield new HillClimbingExploreStrategy();
+            }
+            case RandomWalk -> {
+                yield new RandomWalkExploreStrategy();
+            }
+            default -> throw new IllegalArgumentException("Unknown strategy: " + strategyName);
+        };
+    }
 }
